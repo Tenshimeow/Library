8a6be09a05d9e841acb8a65e87d4fc87fc866220
Hoang Ngoc Hoan <NgochoanDK372005@gmail.com>
2026-06-24 16:06:55 +0000

    Hoan da commit

--- book.php
+++ book.php
@@ -15,7 +15,7 @@ if(!isset($_SESSION['username'])){
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
     <style>
-        /* --- STYLE GIAO DIỆN ĐỒ HỌA GAME RETRO CHỨC NĂNG (BOOK) --- */
+        /* --- STYLE GIAO DIỆN ĐỒ HỌA GAME RETRO CHỨC NĂNG --- */
         * { 
             box-sizing: border-box; 
             margin: 0; 
