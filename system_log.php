4904079dc4a0df26186e0a0ef31fa4859ad45bc3
nguyenngochuy <ngochuy@gmail.com>
2026-06-24 17:04:20 +0000

    thay doi tieu de

--- system_log.php
+++ system_log.php
@@ -12,7 +12,7 @@ $role = $_SESSION['role'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nhật ký hệ thống - Library System</title>
+    <title>Nhật ký hệ thống cua nguyenngochuy - Library System</title>
     <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
